51487b20c40de5f6329bfecabe7f332c577c1ba9
00b7ce <sleep.seep.sheep@gmail.com>
2023-03-06 14:42:28 +0900

    ugokutokoromade

--- scripts/zpreset_utils.py
+++ scripts/zpreset_utils.py
@@ -25,17 +25,14 @@ class PresetManager(scripts.Script):
 
     BASEDIR = scripts.basedir()
 
-    def update_component_name(self, preset, oldval, newval):
-        if preset.get(oldval) is not None:
-            preset[newval] = preset.pop(oldval)
-
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
         self.compinfo = namedtuple("CompInfo", ["component", "label", "elem_id", "kwargs"])
 
         self.settings_file = "presets.json"
 
-        self.available_components = [
+        self.txt2img_component_ids = [
             "txt2img_prompt",
             "txt2img_neg_prompt",
             "txt2img_sampling",
@@ -55,139 +52,95 @@ class PresetManager(scripts.Script):
             "txt2img_batch_size",
             "txt2img_cfg_scale",
         ]
-        
-        # components that pass through after_components
-        self.all_components = []
 
-        # Read saved settings
-        PresetManager.all_presets = self.get_config(self.settings_file)
+        self.txt2img_component_map = {k: None for k in self.txt2img_component_ids}
 
-        # Initialize
-        self.component_map = {k: None for k in self.available_components}
+        self.txt2img_config_presets = {
+            "Default": {},
+            "Low quality ------ 512x512, steps: 10, batch size: 8, DPM++ 2M Karras": {
+                "txt2img_sampling": "DPM++ 2M Karras",
+                "txt2img_steps": 10,
+                "txt2img_width": 512,
+                "txt2img_height": 512,
+                "txt2img_batch_count": 1,
+                "txt2img_batch_size": 8,
+                "txt2img_cfg_scale": 7,
+            },
+        }
 
     def fakeinit(self, *args, **kwargs):
-        """
-        __init__ workaround, since some data is not available during instantiation, such as is_img2img, filename, etc.
-        This method is called from .show(), as that's the first method ScriptRunner calls after handing some state dat (is_txt2img, is_img2img2)
-        """
-        self.elm_prfx = "preset-util"
+        self.elm_prfx = "quick_preset"
 
         if self.is_txt2img:
-            PresetManager.txt2img_preset_dropdown = gr.Dropdown(
+            PresetManager.t2i_preset_dropdown = gr.Dropdown(
                 label="",
-                choices=list(PresetManager.all_presets.keys()),
+                choices=list(self.txt2img_config_presets.keys()),
                 render = False,
-                elem_id=f"{self.elm_prfx}_preset_qs_dd"
+                elem_id=f"{self.elm_prfx}_preset_dd"
             )
-        # else:
-            PresetManager.img2img_preset_dropdown = gr.Dropdown(
-                label="Presets",
-                choices=list(PresetManager.all_presets.keys()),
-                render = False,
-                elem_id=f"{self.elm_prfx}_preset_qs_dd"
-            )
-
-        self.stackable_check = gr.Checkbox(value=True, label="Stackable", elem_id=f"{self.elm_prfx}_stackable_check", render=False)
-        self.save_as = gr.Text(render=False, label="Save", elem_id=f"{self.elm_prfx}_save_qs_txt")
-        self.save_button = gr.Button(value="💾", variant="secondary", render=False, visible=True, elem_id=f"{self.elm_prfx}_save_qs_txt")
+        self.save_as     = gr.Text(render=False, label="Save", elem_id=f"{self.elm_prfx}_save")
+        self.save_button = gr.Button(value="💾", variant="secondary", render=False, visible=True, elem_id=f"{self.elm_prfx}_save")
 
     def title(self):
-        return "Presets"
-
+        return "Quick Preset"
+    
     def show(self, is_img2img):
         self.fakeinit()
-        return True
-
-    def before_component(self, component, **kwargs):
-        pass
-
+        return scripts.AlwaysVisible
+    
     def after_component(self, component, **kwargs):
 
-        if hasattr(component, "label") or hasattr(component, "elem_id"):
-            self.all_components.append(self.compinfo(
-                                                    component=component,
-                                                    label=component.label if hasattr(component, "label") else None,
-                                                    elem_id=component.elem_id if hasattr(component, "elem_id") else None,
-                                                    kwargs=kwargs
-                                                    )
-                                        )
-        label = kwargs.get("label")
-        ele = kwargs.get("elem_id")
-        if label in self.component_map:
-            self.component_map.update({component.label: component})
-        
-        if ele == "txt2img_clear_prompt" or ele == "img2img_clear_prompt":
-            if self.is_txt2img:
-                PresetManager.txt2img_preset_dropdown.render()
-            else:
-                PresetManager.img2img_preset_dropdown.render()
+        self.component_map = self.txt2img_component_map
+        self.component_ids = self.txt2img_component_ids
+
+        if component.elem_id in self.component_map:
+            self.component_map[component.elem_id] = component
+
+        if component.elem_id == "txt2img_generation_info_button":
+            for component_name, component in self.component_map.items():
+                if component is None:
+                    print(f"[ERROR][Config-Presets] The component '{component_name}' no longer exists in the Web UI. Try updating the Config-Presets extension. This extension will not work until this issue is resolved.")
+                    return
+
+            # Mark components with type "index" to be transform
+            self.index_type_components = []
+            for component in self.component_map.values():
+                if getattr(component, "type", "No type attr") == "index":
+                    self.index_type_components.append(component.elem_id)
             self._ui()
 
-        if ele == "txt2img_styles" or ele == "img2img_styles":
+        if component.elem_id == "txt2img_clear_prompt":
+            PresetManager.t2i_preset_dropdown.render()
+
+        if component.elem_id == "txt2img_styles":
             self.save_as.render()
             self.save_button.render()
-            self._ui2()
 
-    def ui(self, *args):
-        pass
+    def preset_dropdown_change(self, selector, *components):
+        config_preset = self.txt2img_config_presets[selector]
+        current_components = dict(zip(self.component_map.keys(), components))
+        current_components.update(config_preset)
 
-    #####TODO
-    def preset_dropdown_change(self, selection, *comps_vals):
-        print(self.component_map.keys())
+        for component_name, component_value in current_components.items():
+            if component_name in self.index_type_components and type(component_value) == int:
+                current_components[component_name] = self.component_map[component_name].choices[component_value]
+
+        return list(current_components.values())
 
     def _ui(self):
-        if self.is_txt2img:
-            PresetManager.txt2img_preset_dropdown.change(
-                fn=self.preset_dropdown_change,
-                inputs=[],
-                outputs=[],
-            )
-            PresetManager.txt2img_preset_dropdown.change(
-                fn=None,
-                inputs=[],
-                outputs=[],
-                _js="config_preset_dropdown_change",
-            )
-        else:
-            PresetManager.img2img_preset_dropdown.change(
-                fn=self.preset_dropdown_change,
-                inputs=[],
-                outputs=[],
-            )
-    def _ui2(self):
-        self.save_button.click(
-            fn=self.save_config,
-            inputs=self.save_as,
-            outputs=[]
+        components = list(self.component_map.values())
+        PresetManager.t2i_preset_dropdown.change(
+            fn = self.preset_dropdown_change,
+            inputs = [PresetManager.t2i_preset_dropdown, *components],
+            outputs = components
         )
-
         self.save_as.change(
             fn = lambda x: gr.update(variant = "primary" if bool(x) else "secondary"),
             inputs = self.save_as,
             outputs = self.save_button
         )
-
-    def run(self, p, *args):
-        pass
-
-    #####TODO
-    def save_config(self, config_name):
-        print(config_name)
-
-    def f_b_syncer(self):
-        """
-        ?Front/Backend synchronizer?
-        Not knowing what else to call it, simple idea, rough to figure out. When updating choices on the front-end, back-end isn't updated, make them both match
-        https://github.com/gradio-app/gradio/discussions/2848
-        """
-        self.inspect_dd.choices = [str(x) for x in self.all_components]
-        return [gr.update(choices=[str(x) for x in self.all_components]), gr.Button.update(visible=False)]
-
-    def get_config(self, path, open_mode='r'):
-        file = os.path.join(PresetManager.BASEDIR, path)
-        try:
-            with open(file, open_mode) as f:
-                as_dict = json.load(f) 
-        except FileNotFoundError as e:
-            print(f"{e}\n{file} not found, check if it exists or if you have moved it.")
-        return as_dict
+        self.save_button.click(
+            fn=None,
+            inputs=[],
+            outputs=[]
+        )
